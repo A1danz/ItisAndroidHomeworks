ffbd0ee01ba2c78f9f3c5b1866579d42168800a9
A1danz <galeevaidan@yandex.ru>
2024-03-07 12:35:45 +0300

    base dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,25 +39,32 @@ android {
 }
 
 dependencies {
-    val nav_version = "2.7.5"
+    val coreKtxVersion = "1.12.0"
+    implementation("androidx.core:core-ktx:$coreKtxVersion")
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    val appCompatVersion = "1.6.1"
+    implementation("androidx.appcompat:appcompat:$appCompatVersion")
+
+    val materialVersion = "1.11.0"
+    implementation("com.google.android.material:material:$materialVersion")
+
+    val viewBindingDelegateVersion = "1.5.9"
+    implementation("com.github.kirich1409:viewbindingpropertydelegate-noreflection:$viewBindingDelegateVersion")
+
+    val lifecycleVersion = "2.7.0"
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+
+    val navVersion = "2.7.5"
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$navVersion")
+
+
+    
+
+    // Test section
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    
-    // Kotlin
-    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
-    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
-    // Feature module Support
-    implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
-    // Testing Navigation
-    androidTestImplementation("androidx.navigation:navigation-testing:$nav_version")
-    // Jetpack Compose Integration
-    implementation("androidx.navigation:navigation-compose:$nav_version")
 
 }
